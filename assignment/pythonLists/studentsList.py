5edc546dfed2e8dd8537117c1a84b95ab70001f0
grg-rohit <grgrohit746@gmail.com>
2021-07-09 20:31:52 +0545

    Name of students starting with A and B

---
+++ assignment/pythonLists/studentsList.py
@@ -0,0 +1,16 @@
+#list of ten students
+studentList = ["Kamal Sheel", "Nirayu", "Aasish", "Kashyap", "Siddartha", "Shenang", "Amit", "Pratik", "Sonish", "Albin"]
+
+#students starting with B
+for student in studentList:
+    if (student.startswith("B")):
+        print(student)
+       
+
+
+#students starting with A
+for student in studentList:   
+    if (student.startswith("A")):
+        print(student)
+
+
