761c54f4053848765a79cce77df847e409de5eae
grg-rohit <grgrohit746@gmail.com>
2021-07-09 20:21:29 +0545

    capitalizing the first alphabets of the elemnts in a list

---
+++ assignment/pythonLists/capitalizing.py
@@ -0,0 +1,5 @@
+#changing the first alphabet of all elements of the following list to capital
+Bob = ["hurricane", "tambourine", "blowing", "numb"]
+
+for element in Bob:
+    print(element.title())
