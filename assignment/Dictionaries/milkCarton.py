f7dfef99e6dd1b9035ce68aa0b3c5bb49e9d23ec
grg-rohit <grgrohit746@gmail.com>
2021-07-19 16:28:20 +0545

    Creating an empty dictionary called milk_carton and adding keys and values in it. printing values of all of the elements of the milk_carton using the values in the dictionary

--- assignment/Dictionaries/milkCarton.py
+++ assignment/Dictionaries/milkCarton.py
@@ -1,13 +1,13 @@
-milkCarton = {}
+milk_carton = {}
 
 #adding keys and values in milk Carton dictionary
-milkCarton['Expiration_date'] = (20, 12, 2022)
-milkCarton['Vol'] = 2
-milkCarton['Cost'] = 45
-milkCarton['Brand_name'] = "D.D.C"
+milk_carton['Expiration_date'] = (20, 12, 2022)
+milk_carton['Vol'] = 2
+milk_carton['Cost'] = 45
+milk_carton['Brand_name'] = "D.D.C"
 
-print(milkCarton)
+print(milk_carton)
 
 #printing values of all of the elements of the milk_carton using the values in the dictionary
-for value in milkCarton.values():
+for value in milk_carton.values():
     print(value)
