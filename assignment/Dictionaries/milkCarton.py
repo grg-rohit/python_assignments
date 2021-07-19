5253461312a76d287de684f9b21eb90d3675a423
grg-rohit <grgrohit746@gmail.com>
2021-07-19 16:33:15 +0545

    Creating an empty dictionary called milk_carton and adding keys and values in it. Printing values of all of the elements of the milk_carton using the values in the dictionary and calculating the  cost of six cartons of milk based on the cost of the milk_carton.

--- assignment/Dictionaries/milkCarton.py
+++ assignment/Dictionaries/milkCarton.py
@@ -3,11 +3,15 @@ milk_carton = {}
 #adding keys and values in milk Carton dictionary
 milk_carton['Expiration_date'] = (20, 12, 2022)
 milk_carton['Vol'] = 2
-milk_carton['Cost'] = 45
+milk_carton['Cost'] = 50
 milk_carton['Brand_name'] = "D.D.C"
 
-print(milk_carton)
+print("\n", milk_carton, "\n")
 
 #printing values of all of the elements of the milk_carton using the values in the dictionary
 for value in milk_carton.values():
     print(value)
+
+
+cost = milk_carton['Cost'] * 6
+print("\nCost of 6 cartons of milk is", cost, "\n")
