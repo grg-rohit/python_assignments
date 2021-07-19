d7bf1dd45f276735139261639ea8ee6fe65d3f59
grg-rohit <grgrohit746@gmail.com>
2021-07-19 09:06:46 +0545

    patterns

---
+++ assignment/For/pattern.py
@@ -0,0 +1,9 @@
+rows = 6
+# Outer loop will print number of rows  
+for i in range(0,rows+1):  
+# Inner loop will print number of Astrisk  
+  for j in range(i):  
+    print("FF", end = "")  
+  print("F") 
+
+
