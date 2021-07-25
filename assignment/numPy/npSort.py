40cb38d69c0356bb5f6fc9be8d0958b2a00cf134
grg-rohit <grgrohit746@gmail.com>
2021-07-25 13:57:46 +0545

    NumPy program to sort a given array of shape 2 along the first axis, last axis and on a flattened array.

---
+++ assignment/numPy/npSort.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([[10,40],[30,20]])
+print("Original array:")
+print(a)
+print("Sort the array along the first axis:")
+print(np.sort(a, axis=0))
+print("Sort the array along the last axis:")
+print(np.sort(a))
+print("Sort the flattened array:")
+print(np.sort(a, axis=None))
