523d77f36fe48d135f2c56d1072b08ca68ebeb60
grg-rohit <grgrohit746@gmail.com>
2021-07-25 13:00:47 +0545

    NumPy program to create an array of 10 zeros,10 ones, 10 fives.

---
+++ assignment/numPy/arrayCreation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+#creating an array of 10 0s
+x = np.zeros(10)
+
+#creating an array of 10 1s
+y = np.ones(10)
+
+#creating an array of 10 5s
+z =np.ones(10) * 5
+
+print("An array of 10 0s: ",x)
+
+print("An array of 10 1s: ",y)
+
+print("An array of 10 5s: ",z)
