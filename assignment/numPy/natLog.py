6592b8f7facaaeab09b766f8cac80fb840b28d9a
grg-rohit <grgrohit746@gmail.com>
2021-07-25 14:38:48 +0545

    NumPy program to compute natural, base 10, and base 2 logarithms for all elements in a given array.

---
+++ assignment/numPy/natLog.py
@@ -0,0 +1,7 @@
+import numpy as np
+A = [1,2.5,56,19]
+print("Original array: ")
+print(A)
+print("\nNatural log =", np.log(A))
+print("Common log =", np.log10(A))
+print("Base 2 log =", np.log2(A))
